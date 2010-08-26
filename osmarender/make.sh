5fec3993558e085b0e89902dae59dfa508da0770
MrTweek <mrtweek@flupps.net>
2010-08-26 16:04:47 +0200

    more svg to webroot

--- osmarender/make.sh
+++ osmarender/make.sh
@@ -2,4 +2,6 @@
 # xsltproc -o durlach.svg osmarender.xsl osm-map-features-z17.xml
 # xsltproc -o durlach.svg osmarender.xsl blank-rel.xml
 xsltproc -o durlach.svg osmarender.xsl citymap-ped.xml
+
+mv durlach.svg ~/public_html/osm/
 # inkscape -d 900 -D -e  durlach.png durlach.svg
